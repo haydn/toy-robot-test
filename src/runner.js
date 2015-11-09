117c837f06849142ef8000da0c140c3b6b71808a
Haydn Ewers <haydn@particlesystem.com>
2015-11-09 15:54:14 +1030

    Make the Runner handle place and report orders.

--- src/runner.js
+++ src/runner.js
@@ -1,7 +1,45 @@
+/*eslint no-console: 0*/
+
+import fs from 'fs';
+import Position from './position';
+import Robot from './robot';
+import Table from './table';
+
 export default class Runner {
 
-  exec() {
-    return;
+  constructor() {
+    this._table = new Table(5, 5);
+    this._robot = new Robot(this._table);
+  }
+
+  exec(file, callback) {
+
+    fs.readFile(file, 'utf8', (err, orders) => {
+
+      let output = [];
+
+      orders.split('\n').map(x => x.trim()).forEach(order => {
+
+        let [type, data] = order.split(' ').map(x => x.trim());
+
+        switch (type.toUpperCase()) {
+          case 'PLACE':
+            let [x, y, facing] = (data || '').split(',');
+            this._robot.place(new Position(x, y, facing));
+            break;
+          case 'REPORT':
+            output.push(this._robot.position.report());
+            break;
+          default:
+            console.log('Unknown order:', type);
+        }
+
+      });
+
+      callback(output.join('\n'));
+
+    });
+
   }
 
 }
--- test/runner-test.js
+++ test/runner-test.js
@@ -2,6 +2,18 @@ import test from 'tape';
 import path from 'path';
 import Runner from '../src/runner';
 
+test('Runner#exec must read PLACE and REPORT orders from a file and output the results', t => {
+
+  let runner = new Runner();
+
+  t.plan(1);
+
+  runner.exec(path.join(__dirname, 'resources/basic.txt'), output => {
+    t.equals(output.trim(), '1,1,NORTH');
+  });
+
+});
+
 test.skip('Runner#exec must read orders from a file, pass them on to the robot and output the results', t => {
 
   let runner = new Runner();
